1d3fdac54f5db15a94819a1cf415a1227b781265
Henk Houwaard <henkhouwaard@gmail.com>
2017-11-12 20:44:17 +0100

    Add test

--- problem1/listcomprehension.hs
+++ problem1/listcomprehension.hs
@@ -1,5 +1,8 @@
 --If we list all the natural numbers below 10 that are multiples
 --of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 --Find the sum of all the multiples of 3 or 5 below 1000.
-
+import Test.HUnit
 problem1 = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 ==0]
+
+test1 = TestCase (assertEqual "problem1," 233168 problem1)
+tests = TestList [TestLabel "test1" test1]
